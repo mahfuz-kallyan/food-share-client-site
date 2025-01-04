f85782a58d3c5c891da1b7eca395dda07d4f7280
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2025-01-04 18:19:35 +0600

    added add food form

--- src/Pages/AddFoods.jsx
+++ src/Pages/AddFoods.jsx
@@ -1,11 +1,144 @@
 import React from 'react';
 
 const AddFoods = () => {
+
+    const handleAddFood = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const initialData = Object.fromEntries(formData.entries());
+        console.log(initialData);
+        
+    }
+
     return (
-        <div>
-            <h2 className="text-3xl">Add Foods</h2>
-        </div>
-    );
+		<div className="mx-auto">
+			<h2 className="text-3xl">Add Foods</h2>
+			<div>
+				<form onSubmit={handleAddFood} className="card-body">
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Food Name</span>
+						</label>
+						<input
+							type="text"
+							placeholder="name"
+							name="name"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Photo</span>
+						</label>
+						<input
+							type="text"
+							placeholder="photo url"
+							name="photo"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Food Quantity</span>
+						</label>
+						<input
+							type="text"
+							placeholder="quantity"
+							name="quantity"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Pickup Location</span>
+						</label>
+						<input
+							type="text"
+							placeholder="location"
+							name="location"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Food Status</span>
+						</label>
+						<input
+							type="text"
+							placeholder="status"
+							name="status"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Expiration Date</span>
+						</label>
+						<input
+							type="date"
+							name="expireDate"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Donator Name</span>
+						</label>
+						<input
+							type="text"
+							placeholder="donator name"
+							name="donator"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Donator Image</span>
+						</label>
+						<input
+							type="text"
+							placeholder="image"
+							name="image"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Donator Email</span>
+						</label>
+						<input
+							type="email"
+							placeholder="email"
+							name="email"
+							className="input input-bordered"
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label className="label">
+							<span className="label-text">Additional notes</span>
+						</label>
+						<textarea
+							className="textarea textarea-bordered"
+							placeholder="notes"
+							name="notes"
+						></textarea>
+					</div>
+					<div className="form-control mt-6">
+						<button className="btn btn-primary">Add Food</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default AddFoods;
