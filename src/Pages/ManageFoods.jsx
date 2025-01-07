5f9a53e09be923c64dfcb01f6723f9d1081f57d0
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2025-01-08 02:16:30 +0600

    added updated function

--- src/Pages/ManageFoods.jsx
+++ src/Pages/ManageFoods.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useContext } from 'react';
 import AuthContext from '../Context/AuthContext/AuthContext';
+import { toast } from 'react-toastify';
 
 const ManageFoods = () => {
     const {user} = useContext(AuthContext)
@@ -14,11 +15,33 @@ const ManageFoods = () => {
         }
         
     })
+
+    const handleStatus = (e, id) => {
+       console.log(e.target.value, id);
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/manage-foods/${id}`, {
+			method: "PATCH",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+                if (data.modifiedCount) {
+                   toast.success('Updated Status Successfully')
+               }
+                
+            })
+        .catch(error => console.log(error)
+        )
+        
+    }
     return (
 		<div className="mx-auto my-8">
-			<h2 className="text-4xl font-semibold text-center">
-				Manage Foods
-			</h2>
+			<h2 className="text-4xl font-semibold text-center">Manage Foods</h2>
 			<div className="p-2 lg:p-12 mt-4">
 				<div className="overflow-x-auto">
 					<table className="table">
@@ -28,7 +51,8 @@ const ManageFoods = () => {
 								<th></th>
 								<th>Food Name</th>
 								<th>Notes</th>
-								<th>Quantity</th>
+								<th>Status</th>
+								<th>Update Status</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -37,11 +61,21 @@ const ManageFoods = () => {
 									<th>{index + 1}</th>
 									<td>{food.name}</td>
 									<td>{food.notes}</td>
-									<td>{food.quantity}</td>
+									<td>{food.status}</td>
 									<td>
-										<button className="btn text-[#94c341] border-[#94c341]">
-											Update
-										</button>
+										<select
+											onChange={(e) => handleStatus(e, food._id)}
+											defaultValue={
+												food.status || "Change Status"
+											}
+											className="select select-bordered select-xs w-full max-w-xs"
+										>
+											<option disabled>
+												Change Status
+											</option>
+											<option>Available</option>
+											<option>Not Available</option>
+										</select>
 									</td>
 									<td>
 										<button className="btn btn-neutral">
