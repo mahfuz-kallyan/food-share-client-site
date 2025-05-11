333aea0793f0f98838cfd8ec2f989b43c15a5360
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2025-05-11 13:05:12 +0600

    added image

--- src/Pages/AvailableFoods.jsx
+++ src/Pages/AvailableFoods.jsx
@@ -6,15 +6,17 @@ import FoodsCard from "../Components/FoodsCard";
 const AvailableFoods = () => {
     const foods = useLoaderData()
     return (
-        <div className="mx-auto">
-            <h2 className="text-4xl font-semibold my-8 text-center">Available foods</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto my-14">
-                {
-                    foods.map(food => <FoodsCard key={food._id} food={food}></FoodsCard>)
-                }
-            </div>
-        </div>
-    );
+		<div className="mx-auto">
+			<h2 className="text-4xl font-semibold my-8 text-center">
+				Available foods
+			</h2>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 w-11/12 mx-auto my-14">
+				{foods.map((food) => (
+					<FoodsCard key={food._id} food={food}></FoodsCard>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default AvailableFoods;
