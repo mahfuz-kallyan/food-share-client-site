fa08cb2bc1c5ceafd5f20358893f1d55ba1396db
mahfuz-kallyan <mahfuzkallyan@gmail.com>
2025-05-12 14:38:46 +0600

    added font size

--- src/Pages/Home/Delivery.jsx
+++ src/Pages/Home/Delivery.jsx
@@ -3,12 +3,12 @@ import deliverLottie from '../../assets/Lottie/Animation - 1747036207150.json'
 
 const Delivery = () => {
     return (
-		<div className="flex flex-col lg:flex-row justify-center items-center p-6 lg:p-14 gap-8">
+		<div className="flex flex-col-reverse lg:flex-row justify-center items-center p-6 lg:p-14 gap-2 lg:gap-8">
 			<div className="w-[500px]">
 				<Lottie animationData={deliverLottie}></Lottie>
 			</div>
 			<div className="space-y-2">
-				<h4 className="text-2xl text-[#94c341]">At your service</h4>
+				<h4 className="text-2xl font-semibold text-[#94c341]">At your service</h4>
 				<h2 className="text-4xl font-semibold text-orange-600">
 					Free Delivery
 				</h2>
